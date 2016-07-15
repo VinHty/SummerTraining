d5806df06f33661777922ded7324a7d06ab2d1fe
leizhen <12345678@qq.com>
2016-07-15 22:25:53 +0800

    2016.7.15

--- app/src/main/java/com/example/jucc/summertraining/JobDetailsAcitiviy.java
+++ app/src/main/java/com/example/jucc/summertraining/JobDetailsAcitiviy.java
@@ -1,9 +1,10 @@
 package com.example.jucc.summertraining;
 
+import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class JobDetailsAcitiviy extends AppCompatActivity {
+public class JobDetailsAcitiviy extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
