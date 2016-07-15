b91ceab30e5943ac59aae3b449caf675750ddf9e
Frank <283668212@qq.com>
2016-07-15 15:18:58 +0800

    AI-2.1 <jucc@juccdeMacBook-Pro.local Merge branch 'master'

--- app/src/main/java/com/example/jucc/summertraining/MainActivity.java
+++ app/src/main/java/com/example/jucc/summertraining/MainActivity.java
@@ -1,25 +1,65 @@
 package com.example.jucc.summertraining;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+
+import static com.example.jucc.summertraining.R.styleable.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
+private Button queryJob;
+    private Button queryTiming;
+    private  Button shop;
+    private Button setting;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        toolbar.setTitle("leizhenshadiao");
+        Button queryJob = (Button)findViewById(R.id.job_query);
+        Button queryTiming = (Button)findViewById(R.id.timing_query);
+        Button shop = (Button)findViewById(R.id.shop);
+        Button setting = (Button)findViewById(R.id.setting);
 
-
+        queryJob.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, JobDetailsAcitiviy.class);
+                MainActivity.this.startActivity(intent);
+                MainActivity.this.finish();
+            }
+        });
+        queryTiming.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, AppUsageAmountAcitivity.class);
+                MainActivity.this.startActivity(intent);
+                MainActivity.this.finish();
+            }
+        });
+        setting.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, SettingsActivity.class);
+                MainActivity.this.startActivity(intent);
+                MainActivity.this.finish();
+            }
+        });
+        shop.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, ShopActivity.class);
+                MainActivity.this.startActivity(intent);
+                MainActivity.this.finish();
+            }
+        });
         
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
