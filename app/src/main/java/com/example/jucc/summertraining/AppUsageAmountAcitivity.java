35f3fd3dcdc2e61e4a9a4ea38b14bc95405532bd
Vin <791606691@qq.com>
2016-07-15 14:33:43 +0800

    ScreenLisnter done ; Job entity done;

--- app/src/main/java/com/example/jucc/summertraining/AppUsageAmountAcitivity.java
+++ app/src/main/java/com/example/jucc/summertraining/AppUsageAmountAcitivity.java
@@ -18,7 +18,7 @@ import android.view.ViewGroup;
 
 import android.widget.TextView;
 
-public class AppUsageAmount extends AppCompatActivity {
+public class AppUsageAmountAcitivity extends AppCompatActivity {
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
--- app/src/main/java/com/example/jucc/summertraining/Entity/Job.java
+++ app/src/main/java/com/example/jucc/summertraining/Entity/Job.java
@@ -5,4 +5,63 @@ package com.example.jucc.summertraining.Entity;
  */
 
 public class Job {
+    private String title;
+    private String lastTime;
+    private String beginTime;
+    private boolean success;
+    private String remindTime;
+
+    public Job(String title, String lastTime, String beginTime, boolean success, String remindTime) {
+        this.title = title;
+        this.lastTime = lastTime;
+        this.beginTime = beginTime;
+        this.success = success;
+        this.remindTime = remindTime;
+    }
+
+    public Job(String title) {
+        this.title = title;
+    }
+
+    public String getRemindTime() {
+        return remindTime;
+    }
+
+    public void setRemindTime(String remindTime) {
+        this.remindTime = remindTime;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getLastTime() {
+        return lastTime;
+    }
+
+    public void setLastTime(String lastTime) {
+        this.lastTime = lastTime;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBeginTime() {
+        return beginTime;
+    }
+
+    public void setBeginTime(String beginTime) {
+        this.beginTime = beginTime;
+    }
+
+
 }
