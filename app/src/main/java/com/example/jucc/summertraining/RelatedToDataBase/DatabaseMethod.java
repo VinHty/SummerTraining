3eb4871207dddc77ba5864ca7e381e5509d63039
Frank <283668212@qq.com>
2016-07-16 19:05:40 +0800

    AI-2.1 <jucc@juccdeMacBook-Pro.local Merge branch 'master'

--- app/src/main/java/com/example/jucc/summertraining/RelatedToDataBase/DatabaseMethod.java
+++ app/src/main/java/com/example/jucc/summertraining/RelatedToDataBase/DatabaseMethod.java
@@ -53,9 +53,9 @@ public class DatabaseMethod {
     /*
     **将今天的使用时间插入到总的时间记录表中
         突然发现这个函数没用……尴尬
-    public void  update_usetime(SQLiteDatabase db,String appname,int use,String date){
+    public void  update_usetime(String appName,int use,String date){
         String sqlupdate;
-        sqlupdate="update usetime set use_time='"+use+"' where app_name='"+appname+"' and use_date='"+date+"'";
+        sqlupdate="update usetime set use_time='"+use+"' where app_name='"+appName+"' and use_date='"+date+"'";
         db.execSQL(sqlupdate);
     }
     */
@@ -63,58 +63,71 @@ public class DatabaseMethod {
             /*
     **开始自定义任务时插入
     */
-    public void insert_quickjob(String timeStamp,String jobname,int lasttime,String starttime){
+    public void insert_quickjob(String timeStamp,String jobName,int lastTime,String startTime){
         String sqlinsert;
-        sqlinsert="insert into job( set_time,job_name,last_time,start_time,is_suc,alert_time) values('"+timeStamp+"','"+jobname+"','"+lasttime+"','"+starttime+"','NULL','NULL')";
-  //      db.execSQL(sqlinsert);
+        sqlinsert="insert into job( set_time,job_name,last_time,start_time,is_suc,alert_time) values('"+timeStamp+"','"+jobName+"','"+lastTime+"','"+startTime+"','NULL','NULL')";
+        db.execSQL(sqlinsert);
     }
     /*
     **添加没有提醒任务时插入
     */
-    public void insert_jobWithoutAlert(String timeStamp,String jobname){
+    public void insert_jobWithoutAlert(String timeStamp,String jobName){
         String sqlinsert;
-        sqlinsert="insert into job( set_time,job_name,last_time,start_time,is_suc,alert_time) values('"+timeStamp+"','"+jobname+"','NULL','NULL','NULL','NULL')";
-     //   db.execSQL(sqlinsert);
+        sqlinsert="insert into job( set_time,job_name,last_time,start_time,is_suc,alert_time) values('"+timeStamp+"','"+jobName+"','NULL','NULL','NULL','NULL')";
+        db.execSQL(sqlinsert);
     }
 
     /*
     **添加有提醒任务时插入
     */
-    public void insert_jobWithAlert(String timeStamp,String jobname,String alerttime){
+    public void insert_jobWithAlert(String timeStamp,String jobName,String alertTime){
         String sqlinsert;
-        sqlinsert="insert into job( set_time,job_name,last_time,start_time,is_suc,alert_time) values('"+timeStamp+"','"+jobname+"','NULL','NULL','NULL','"+alerttime+"')";
-     //   db.execSQL(sqlinsert);
+        sqlinsert="insert into job( set_time,job_name,last_time,start_time,is_suc,alert_time) values('"+timeStamp+"','"+jobName+"','NULL','NULL','NULL','"+alertTime+"')";
+        db.execSQL(sqlinsert);
     }
 
     /*
     **编辑任务时更新
     */
-    public void update_jobWhenEdit(String timeStamp,String jobname,String alerttime){
+    public void update_jobWhenEdit(String timeStamp,String jobName,String alertTime){
         String sqlupdate;
-        sqlupdate="update job set job_name='"+jobname+"',alert_time='"+alerttime+"' where set_time='"+timeStamp+"'";
-    //    db.execSQL(sqlupdate);
+        sqlupdate="update job set job_name='"+jobName+"',alert_time='"+alertTime+"' where set_time='"+timeStamp+"'";
+        db.execSQL(sqlupdate);
     }
     /*
     **开始任务时更新
     */
-    public void update_jobWhenStart(String jobname,int lasttime,String starttime){
+    public void update_jobWhenStart(String jobName,int lastTime,String startTime){
         String sqlupdate;
-        sqlupdate="update job set last_time='"+lasttime+"',start_time='"+starttime+"' where job_name='"+jobname+"'";
-    //    db.execSQL(sqlupdate);
+        sqlupdate="update job set last_time='"+lastTime+"',start_time='"+startTime+"' where job_name='"+jobName+"'";
+        db.execSQL(sqlupdate);
     }
     /*
     **结束任务时更新
     */
-  /*  public void update_jobWhenFinish(String timeStamp,boolean issuc){
+    public void update_jobWhenFinish(String timeStamp,boolean isSuc){
         String sqlupdate;
-
-        sqlupdate="update job set is_suc='"+i+"',start_time='"+starttime+"' where job_name='"+jobname+"'";
+        int i;
+        if(isSuc=true){
+            i=1;
+        }
+        else
+        i=0;
+        sqlupdate="update job set is_suc='"+i+"' where set_time='"+timeStamp+"'";
         db.execSQL(sqlupdate);
-    }*/
+    }
+    /*
+    **删除任务
+     */
+    public void delete_job(String timeStamp){
+        String sqldelete;
+        sqldelete="delete from job  where set_time='"+timeStamp+"'";
+        db.execSQL(sqldelete);
+    }
 
 
         /*
-    **将当前日期转化为string，精确到天1
+    **将当前日期转化为string，精确到天
     */
     public static String getStringDate() {
         Date currentTime = new Date();
