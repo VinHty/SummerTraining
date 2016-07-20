a8ed1ddf3cdf1078b49cb2df1bf19257f32be082
Vin <791606691@qq.com>
2016-07-20 15:17:36 +0800

    7-20 Vin 2.0

--- app/src/main/java/com/example/jucc/summertraining/WeekFragment.java
+++ app/src/main/java/com/example/jucc/summertraining/WeekFragment.java
@@ -17,19 +17,19 @@ import java.util.List;
  */
 
 public class WeekFragment extends MyFragment {
- //   @Override
-//    public void getUsageAmountFromDatabase() {
-//        List<UseTime> list = method.getLastWeek();
-//        for(int a=0;a<list.size();a++){
-//            UseTime useTime= list.get(a);
-//            HashMap<String,Object> map = new HashMap<>();
-//            String name =useTime.getAppName();
-//            int time = useTime.getUseTime();
-//            map.put("title",name);
-//            map.put("time",time);
-//            this.list.add(map);
-//    }
-
+    @Override
+    public void getUsageAmountFromDatabase() {
+        List<UseTime> list = method.getLastWeek();
+        for (int a = 0; a < list.size(); a++) {
+            UseTime useTime = list.get(a);
+            HashMap<String, Object> map = new HashMap<>();
+            String name = useTime.getAppName();
+            int time = useTime.getUseTime();
+            map.put("title", name);
+            map.put("time", time);
+            this.list.add(map);
+        }
+    }
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
