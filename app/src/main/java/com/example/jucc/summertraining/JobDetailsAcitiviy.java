ae720147d04b3636ffd1a9f8b19fbc6acaac1771
LeiZhen <3422785094@qq.com>
2016-07-25 11:27:04 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/gradle.xml
    #	app/src/main/res/values/strings.xml
    2016.7.25.11:26

--- app/src/main/java/com/example/jucc/summertraining/JobDetailsAcitiviy.java
+++ app/src/main/java/com/example/jucc/summertraining/JobDetailsAcitiviy.java
@@ -1,17 +1,23 @@
 package com.example.jucc.summertraining;
 
 import android.app.AlertDialog;
+import android.app.Fragment;
 import android.app.ListActivity;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 
@@ -25,24 +31,32 @@ import java.util.List;
 import java.util.Map;
 
 //当用户在主界面点击任务按钮的时候，跳转至该界面，即任务界面
-public class JobDetailsAcitiviy extends ListActivity {
+public class JobDetailsAcitiviy extends AppCompatActivity implements View.OnClickListener{
 
-    private List<Map<String, Object>> mData;
     private Button button_return;
     private Button button_add;
-    private Button button_listFinishedJob;
-    private DatabaseMethod dbMethod;
-    private MyAdapter adapter;
+
+    private Button mTabListFinishedJob;
+    private Button mTabListUnfinishedJob;
+
+    private ListFinishedJobFragment mListFinishedJobFragment;
+    private ListUnfinishedJobFragment mListUnFinishedJobFragment;
+
+    private FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_job_details_acitiviy);
 
-        dbMethod = DatabaseMethod.getInstance(this);
         button_return = (Button)findViewById(R.id.activity_job_details_return);
         button_add = (Button) findViewById(R.id.activity_job_details_add);
-        button_listFinishedJob = (Button)findViewById(R.id.activity_job_details_listFinishedJob);
+        fragmentManager = getSupportFragmentManager();
+
+        mTabListFinishedJob = (Button)findViewById(R.id.fragment_list_finished_job_linearlayout) ;
+        mTabListUnfinishedJob = (Button) findViewById(R.id.fragment_list_unfinished_job_linearlayout);
+        mTabListFinishedJob.setOnClickListener(this);
+        mTabListUnfinishedJob.setOnClickListener(this);
 
         //返回按钮的监听事件
         button_return.setOnClickListener(new View.OnClickListener() {
@@ -62,170 +76,112 @@ public class JobDetailsAcitiviy extends ListActivity {
             }
         });
 
-        //列出所有已完成任务的按钮的监听事件
-        button_listFinishedJob.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent listFinishedJob = new Intent(JobDetailsAcitiviy.this,ListFinishedJob.class);
-                startActivity(listFinishedJob);
-                
-            }
-        });
 
-        //构建listView
-        mData = getData();
-        adapter = new MyAdapter(this);
-        setListAdapter(adapter);
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        hideFragments(transaction);
+        mListUnFinishedJobFragment = new ListUnfinishedJobFragment();
+        transaction.add(R.id.id_content, mListUnFinishedJobFragment);
+        transaction.show(mListUnFinishedJobFragment);
+        transaction.commit();
+
 
     }
 
-    //从数据库获得对应数据
-    private List<Map<String, Object>> getData() {
-        List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
-        DatabaseMethod dbMethod = DatabaseMethod.getInstance(this);
-        List<Job> myJobs = dbMethod.getUnfinishJob();  //从数据库读取未完成的任务,以list的形式
-        for(int i = 0; i < myJobs.size(); i++){
-            Map<String,Object> map = new HashMap<String,Object>();
-            map.put("title",myJobs.get(i).getTitle());
-            map.put("timeStamp",myJobs.get(i).getTimeStamp());
-            map.put("alertTime",myJobs.get(i).getRemindTime());
-            map.put("edit","edit" + i);
-            map.put("start","start" + i);
-            map.put("delete","delete" + i);
-            list.add(map);
+    @Override
+    public void onClick(View v){
+        switch ((v.getId()))
+        {
+            case R.id.fragment_list_finished_job_linearlayout:
+                setTabSelection(0);
+                break;
+            case R.id.fragment_list_unfinished_job_linearlayout:
+                setTabSelection(1);
+                break;
+            default:
+                break;
         }
-        return list;
+
     }
 
-    //获取当前日期和时间
-    public String getCurrentTime(){
-        DatabaseMethod db =DatabaseMethod.getInstance(this);
-        return db.getStringSecond();
-     }
 
-    //listview中每个item的布局class
-    public final class ViewHolder{
-        public TextView title;
-        public Button edit;
-        public Button start;
-        public Button delete;
-    }
-
-    //存取数据的adapter
-    public class MyAdapter extends BaseAdapter {
-
-        private LayoutInflater mInflater;
-
-        //adapter的构造函数
-        public MyAdapter(Context context){
-            this.mInflater = LayoutInflater.from(context);
-        }
-
-        @Override
-        public int getCount() {
-            // TODO Auto-generated method stub
-            return mData.size();
-        }
-
-        @Override
-        public Object getItem(int arg0) {
-            // TODO Auto-generated method stub
-            return null;
-        }
-
-        @Override
-        public long getItemId(int arg0) {
-            // TODO Auto-generated method stub
-            return 0;
-        }
-
-        //listview中每行的view
-        @Override
-        public View getView(final int position, View convertView, ViewGroup parent) {
-
-            ViewHolder holder = null;
-            if (convertView == null) {
-                holder=new ViewHolder();
-                convertView = mInflater.inflate(R.layout.activity_job_details_listview_item, null);
-                holder.title = (TextView)convertView.findViewById(R.id.activity_job_details_textview);
-                holder.edit = (Button)convertView.findViewById(R.id.activity_job_details_edit);
-                holder.start = (Button)convertView.findViewById(R.id.activity_job_details_start);
-                holder.delete = (Button)convertView.findViewById(R.id.activity_job_details_delete);
-                convertView.setTag(holder);
-            }else {
-                holder = (ViewHolder)convertView.getTag();
-            }
-
-            //设置listview中每行的文本信息
-            holder.title.setText((String)mData.get(position).get("title"));
-            holder.edit.setText((String)mData.get(position).get("edit"));
-            holder.start.setText((String)mData.get(position).get("start"));
-            holder.delete.setText((String)mData.get(position).get("delete"));
-
-            //监听器记录了所在行，于是绑定到各个控件后能够返回具体的行，以及触发的控件
-            //delete按钮的监听事件
-            holder.delete.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    dbMethod.delete_job(mData.get(position).get("timeStamp").toString());
-                    mData.remove(position);
-                    adapter.notifyDataSetChanged();
+    private void setTabSelection(int index)
+    {
+        // 重置按钮
+        //resetBtn();
+        // 开启一个Fragment事务
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        // 先隐藏掉所有的Fragment，以防止有多个Fragment显示在界面上的情况
+        hideFragments(transaction);
+        switch (index)
+        {
+            case 0:
+                // 当点击了消息tab时，改变控件的图片和文字颜色
+                //((ImageButton) mTabListFinishedJob.findViewById(R.id.fragment_list_finished_job_linearlayout))
+                //        .setImageResource(R.drawable.tab_weixin_pressed);
+                if (mListFinishedJobFragment == null)
+                {
+                    // 如果MessageFragment为空，则创建一个并添加到界面上
+                    mListFinishedJobFragment = new ListFinishedJobFragment();
+                    transaction.add(R.id.id_content, mListFinishedJobFragment);
+                } else
+                {
+                    // 如果MessageFragment不为空，则直接将它显示出来
+                    transaction.show(mListFinishedJobFragment);
                 }
-            });
-
-
-            final EditText text = new EditText(JobDetailsAcitiviy.this);
-            //开始按钮的监听事件
-            holder.start.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-
-                     //弹出窗口获得用户输入的任务时长
-                     new AlertDialog.Builder(JobDetailsAcitiviy.this).setTitle("请输入任务时长：")
-                            .setView(text)
-                            .setPositiveButton("确定", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    Intent startJob = new Intent(JobDetailsAcitiviy.this,ExecutionActivity.class);
-
-                                    //对输入数据的格式进行判断
-                                    if(text.getText().toString().matches("[0-9]||[1-9][0-9]||[1-6][0-9][0-9]||7[0-1][0-9]||720")&&(!text.getText().toString().isEmpty())) {
-                                        long lastTime = Long.valueOf(text.getText().toString()).longValue();
-                                        startJob.putExtra("title", mData.get(position).get("title").toString());
-                                        startJob.putExtra("timeStamp", mData.get(position).get("timeStamp").toString());
-                                        startJob.putExtra("lastTime", lastTime);
-                                        startActivity(startJob);
-                                    }
-                                    else{
-                                        new AlertDialog.Builder(JobDetailsAcitiviy.this).setTitle("FBI WARNING")
-                                                .setMessage("任务时长应为0-720之间的任意一个数字，请重新输入")
-                                                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
-                                                    @Override
-                                                    public void onClick(DialogInterface dialog, int which) {
-                                                        Intent restartIntent = new Intent(JobDetailsAcitiviy.this,JobDetailsAcitiviy.class);
-                                                        startActivity(restartIntent);
-                                                    }
-                                                }).show();
-                                    }
-                                }
-                            })
-                            .setNegativeButton("取消",null)
-                            .show();
+                break;
+            case 1:
+                // 当点击了消息tab时，改变控件的图片和文字颜色
+                //((ImageButton) mTabBtnFrd.findViewById(R.id.btn_tab_bottom_friend))
+                 //       .setImageResource(R.drawable.tab_find_frd_pressed);
+                if (mListUnFinishedJobFragment == null)
+                {
+                    // 如果MessageFragment为空，则创建一个并添加到界面上
+                    mListUnFinishedJobFragment = new ListUnfinishedJobFragment();
+                    transaction.add(R.id.id_content, mListUnFinishedJobFragment);
+                } else
+                {
+                    // 如果MessageFragment不为空，则直接将它显示出来
+                    transaction.show(mListUnFinishedJobFragment);
                 }
-            });
+                break;
+        }
+        transaction.commit();
+    }
 
-            //edit按钮的响应事件
-            holder.edit.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent editIntent = new Intent(JobDetailsAcitiviy.this,AddJobActivity.class);
-                    editIntent.putExtra("timeStamp",mData.get(position).get("timeStamp").toString());
-                    startActivity(editIntent);
-                }
-            });
-            return convertView;
+    /**
+     * 清除掉所有的选中状态。
+     */
+  /*  private void resetBtn()
+    {
+        ((ImageButton) mTabBtnWeixin.findViewById(R.id.btn_tab_bottom_weixin))
+                .setImageResource(R.drawable.tab_weixin_normal);
+        ((ImageButton) mTabBtnFrd.findViewById(R.id.btn_tab_bottom_friend))
+                .setImageResource(R.drawable.tab_find_frd_normal);
+        ((ImageButton) mTabBtnAddress.findViewById(R.id.btn_tab_bottom_contact))
+                .setImageResource(R.drawable.tab_address_normal);
+        ((ImageButton) mTabBtnSettings.findViewById(R.id.btn_tab_bottom_setting))
+                .setImageResource(R.drawable.tab_settings_normal);
+    }
+    */
+
+    /**
+     * 将所有的Fragment都置为隐藏状态。
+     *
+     * @param transaction
+     *            用于对Fragment执行操作的事务
+     */
+    private void hideFragments(FragmentTransaction transaction)
+    {
+        if (mListFinishedJobFragment != null)
+        {
+            transaction.hide(mListFinishedJobFragment);
+        }
+        if (mListUnFinishedJobFragment != null)
+        {
+            transaction.hide(mListUnFinishedJobFragment);
         }
     }
+
 }
 
 
