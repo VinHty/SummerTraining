15784fd7634cd198b09f3c6842cfaa83dbe7bed5
LeiZhen <3422785094@qq.com>
2016-07-28 10:04:04 +0800

    Merge remote-tracking branch 'origin/master'
                                                 # Conflicts:
                                                 #	.idea/gradle.xml
                                                 #	app/src/main/res/values/strings.xml
                                                 2016.7.28,10:03

--- app/src/main/java/com/example/jucc/summertraining/AchievementMediumFishFragment.java
+++ app/src/main/java/com/example/jucc/summertraining/AchievementMediumFishFragment.java
@@ -4,9 +4,22 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.jucc.summertraining.Entity.Fish;
+import com.example.jucc.summertraining.RelatedToDataBase.DatabaseMethod;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 /**
@@ -17,11 +30,14 @@ import android.view.ViewGroup;
  * Use the {@link AchievementMediumFishFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class AchievementMediumFishFragment extends Fragment {
+public class AchievementMediumFishFragment extends ListFragment{
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    private List<Map<String, Object>> mData;
+    private MyAdapter adapter;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -63,6 +79,10 @@ public class AchievementMediumFishFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        mData = getData();
+        adapter = new MyAdapter(getContext());
+        setListAdapter(adapter);
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_achievement_medium_fish, container, false);
     }
@@ -80,8 +100,8 @@ public class AchievementMediumFishFragment extends Fragment {
         if (context instanceof OnFragmentInteractionListener) {
             mListener = (OnFragmentInteractionListener) context;
         } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+        //    throw new RuntimeException(context.toString()
+        //            + " must implement OnFragmentInteractionListener");
         }
     }
 
@@ -104,5 +124,104 @@ public class AchievementMediumFishFragment extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+
+    private List<Map<String, Object>> getData() {
+        List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+        DatabaseMethod dbMethod = DatabaseMethod.getInstance(getContext());
+        List<Fish> myFish = dbMethod.achievementMid();
+        Log.e("DB","list:    "+myFish.size());//从数据库读取未完成的任务,以list的形式
+        for(int i = 0; i < myFish.size(); i++){
+            Map<String,Object> map = new HashMap<String,Object>();
+            map.put("image",myFish.get(i).getId());
+            map.put("name",myFish.get(i).getName());
+            map.put("amount",myFish.get(i).getTimes());
+            list.add(map);
+        }
+        return list;
+    }
+
+    //获取当前日期和时间
+    public String getCurrentTime(){
+        DatabaseMethod db =DatabaseMethod.getInstance(getContext());
+        return db.getStringSecond();
+    }
+
+    //listview中每个item的布局class
+    public final class ViewHolder{
+        public ImageView leftimagerview;
+        public TextView leftfishname;
+        public TextView leftfishamount;
+        public ImageView rightimageview;
+        public TextView rightfishname;
+        public TextView rightfishamount;
+    }
+
+    //存取数据的adapter
+    public class MyAdapter extends BaseAdapter {
+
+        private LayoutInflater mInflater;
+
+        //adapter的构造函数
+        public MyAdapter(Context context){
+            this.mInflater = LayoutInflater.from(context);
+        }
+
+        @Override
+        public int getCount() {
+            // TODO Auto-generated method stub
+            return mData.size();
+        }
+
+        @Override
+        public Object getItem(int arg0) {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        @Override
+        public long getItemId(int arg0) {
+            // TODO Auto-generated method stub
+            return 0;
+        }
+
+        //listview中每行的view
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+
+            ViewHolder holder = null;
+            if (convertView == null) {
+                holder=new ViewHolder();
+                convertView = mInflater.inflate(R.layout.fragment_achievement_medium_fish_listview,null);
+                holder.leftimagerview = (ImageView) convertView.findViewById(R.id.mediumfish_leftimageview);
+                holder.leftfishname = (TextView) convertView.findViewById(R.id.mediumfish_leftname);
+                holder.leftfishamount = (TextView) convertView.findViewById(R.id.mediumfish_lefttextview);
+                holder.rightimageview = (ImageView)convertView.findViewById(R.id.mediumfish_rightimageview);
+                holder.rightfishname = (TextView)convertView.findViewById(R.id.mediumfish_rightname);
+                holder.rightfishamount = (TextView)convertView.findViewById(R.id.mediumfish_righttextview);
+                convertView.setTag(holder);
+            }else {
+                holder = (ViewHolder)convertView.getTag();
+            }
+            if(position == 0){
+                //设置listview中每行的文本信息
+                holder.leftimagerview.setImageResource((int)mData.get(0).get("image"));
+                Log.e("image resource is ","" + mData.get(0).get("image"));
+                holder.leftfishname.setText((String)mData.get(0).get("name"));
+                holder.leftfishamount.setText("has get: "+mData.get(0).get("amount"));
+                holder.rightimageview.setImageResource((int)mData.get(1).get("image"));
+                holder.rightfishname.setText((String)mData.get(1).get("name"));
+                holder.rightfishamount.setText("has get: "+mData.get(1).get("amount"));
+            }else if(position == 1 || position == 2){
+                //设置listview中每行的文本信息
+                holder.leftimagerview.setImageResource((int)mData.get(2*position).get("image"));
+                holder.leftfishname.setText((String)mData.get(2*position).get("name"));
+                holder.leftfishamount.setText("has get: "+mData.get(2*position).get("amount"));
+                holder.rightimageview.setImageResource((int)mData.get(2*position+1).get("image"));
+                holder.rightfishname.setText((String)mData.get(2*position+1).get("name"));
+                holder.rightfishamount.setText("has get: "+mData.get(2*position+1).get("amount"));
+            }
+            return convertView;
+        }
     }
 }
