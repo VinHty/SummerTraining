7bdbb9e1ee62208be414f737e87f0502def69fa3
LeiZhen <3422785094@qq.com>
2016-07-29 14:51:24 +0800

    Merge remote-tracking branch 'origin/master'
                                                 # Conflicts:
                                                 #	.idea/gradle.xml
                                                 #	app/src/main/res/values/strings.xml
                                                 2016.7.29,14:51

--- app/src/main/java/com/example/jucc/summertraining/ExecutionActivity.java
+++ app/src/main/java/com/example/jucc/summertraining/ExecutionActivity.java
@@ -193,7 +193,7 @@ public class ExecutionActivity extends Activity {
                 quickJobFinish.increaseCoins(m);
                 vibrator = (Vibrator)getSystemService(Context.VIBRATOR_SERVICE);
                 long [] pattern = {100,400,100,400};   // 停止 开启 停止 开启
-                vibrator.vibrate(pattern,2);           //重复两次上面的pattern 如果只想震动一次，index设为-1
+                vibrator.vibrate(pattern,-1);           //重复两次上面的pattern 如果只想震动一次，index设为-1
           //      onStop();
                 new AlertDialog.Builder(ExecutionActivity.this).setTitle("成功")//设置对话框标题1
                         .setMessage("任务成功")//设置显示的内容
@@ -241,9 +241,9 @@ public class ExecutionActivity extends Activity {
 
 
         //关闭Activity并且跳转到MainActivity
-        Intent intent = new Intent();
-        intent.setClass(ExecutionActivity.this,MainActivity.class);
-        ExecutionActivity.this.startActivity(intent);
+//        Intent intent = new Intent();
+//        intent.setClass(ExecutionActivity.this,MainActivity.class);
+//        ExecutionActivity.this.startActivity(intent);
         finish();
     }
 
@@ -259,8 +259,5 @@ public class ExecutionActivity extends Activity {
         return state;
     }
 
-    public void onStop(){
-        super.onStop();
-        vibrator.cancel();
-    }
+
 }
